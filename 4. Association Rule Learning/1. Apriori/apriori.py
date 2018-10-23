6d4d03b3cdc0033aa56ee01020447845e38acb53
mohitrocky <mohit.rock2020@gmail.com>
2018-10-23 10:37:47 +0530

    Update apriori.py

--- 4. Association Rule Learning/1. Apriori/apriori.py
+++ 4. Association Rule Learning/1. Apriori/apriori.py
@@ -4,8 +4,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
-# Data Preprocessing
 dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
 transactions = []
 for i in range(0, 7501):
